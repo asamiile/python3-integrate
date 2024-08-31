7c64ad4f6f928f9e6516af7c6109f93fdeb6167c
asamiile <asamin47@gmail.com>
2024-08-31 20:42:40 +0900

    Update Script.

--- app/fetch_semantic_scholar.py
+++ app/fetch_semantic_scholar.py
@@ -2,7 +2,7 @@ import os
 from dotenv import load_dotenv
 import requests
 import urllib.parse
-from datetime import datetime
+from datetime import datetime, timedelta
 
 # .envファイルを読み込む
 load_dotenv()
@@ -13,24 +13,26 @@ API_KEY = os.getenv("SEMANTIC_SCHOLAR_API_KEY")
 # Semantic Scholar APIのエンドポイント
 API_URL = "https://api.semanticscholar.org/graph/v1/paper/search"
 
-# 現在の年を取得
-current_year = datetime.now().year
-
 # 検索クエリの設定
 query = "Aesthetics"
 
 # キーワードリスト
 # keywords = [
 #     "art",
-#     "machine learning",
-#     "artificial intelligence",
-#     "deep learning",
-#     "AI-generated art",
-#     "Computer Vision"
+#     "Aesthetics",
+    # "machine learning",
+    # "artificial intelligence",
+    # "deep learning",
+    # "AI-generated art",
+    # "Computer Vision"
 # ]
 
 # キーワードをORで結合してクエリを作成
 # query = " OR ".join(keywords)
+
+# publicationDateOrYearパラメータに前日の日付を設定
+today = datetime.now()
+one_day_ago = (today - timedelta(days=1)).strftime('%Y-%m-%d')
 
 # クエリをURLエンコードする
 encoded_query = urllib.parse.quote(query)
@@ -43,10 +45,11 @@ headers = {
 # パラメータの設定
 params = {
     "query": encoded_query,
-    "fields": "title,authors,abstract,year,venue,url",
+    "fields": "title,authors,abstract,tldr,venue,publicationDate,url",
     "fieldsOfStudy": "Art",
+    # 'publicationTypes': 'JournalArticle',
     "limit": 100,
-    'year': f'{current_year}-',
+    "publicationDateOrYear": one_day_ago,
 }
 
 # APIリクエストを送信
@@ -56,14 +59,18 @@ response = requests.get(API_URL, headers=headers, params=params)
 if response.status_code == 200:
     papers = response.json().get("data", [])
 
-    for paper in papers:
-        print(f"Title: {paper.get('title')}")
-        print(f"Authors: {', '.join(author['name'] for author in paper.get('authors', []))}")
-        print(f"Abstract: {paper.get('abstract')}")
-        print(f"Year: {paper.get('year')}")
-        print(f"Venue: {paper.get('venue')}")
-        print(f"URL: {paper.get('url')}")
+    if not papers:
+        print("検索結果がありません")
+    else:
+        for paper in papers:
+            print(f"Title: {paper.get('title')}")
+            print(f"Authors: {', '.join(author['name'] for author in paper.get('authors', []))}")
+            print(f"Abstract: {paper.get('abstract')}")
+            print(f"Tldr: {paper.get('tldr')}")
+            print(f"Venue: {paper.get('venue')}")
+            print(f"Publication Date: {paper.get('publicationDate')}")
+            print(f"URL: {paper.get('url')}")
 
-        print("="*80)
+            print("="*80)
 else:
     print(f"Error: {response.status_code} - {response.text}")
